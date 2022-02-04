d07bd55bfe48c34bba564165126285c0447be6c0
ArseBro228 <iaseshviliarsen@gmail.com>
2022-02-04 19:27:32 +0200

    Lesson_33hw

---
+++ main.js
@@ -0,0 +1,185 @@
+// let a = Number(prompt(`a=`))
+// if(a>0){
+//     alert(a+1)
+// }
+// else if(a<0){
+//     alert(`a<0`)
+// }
+// else{alert(`a=0`)}
+
+// let a = Number(prompt(`a=`))
+// if(a>0){
+//     alert(a+1)
+// }else{alert(a-2)}
+
+// let a = Number(prompt(`a=`))
+// if(a>0){
+//     alert(a+1)
+// }else if(a<0){
+//     alert(a-2)
+// }
+// else{alert(a+10)}
+
+// let a = Number(prompt(`a=`))
+// let b = Number(prompt(`b=`))
+// let c = Number(prompt(`c=`))
+// if (a > 0 && b > 0 && c > 0) {
+//     alert('3')
+// } else if ((a > 0 && b > 0 && c < 0) || (a > 0 && b < 0 && c > 0) || (a < 0 && b > 0 && c > 0)) {
+//     alert("2")
+// } else if ((a > 0 && b < 0 && c < 0) || (a < 0 && b > 0 && c < 0) || (a < 0 && b < 0 && c > 0)) {
+//     alert("1")
+// } else {
+//     alert("0")
+// }
+
+// let a = Number(prompt(`a=`))
+// let b = Number(prompt(`b=`))
+// let c = Number(prompt(`c=`))
+// if (a > 0 && b > 0 && c > 0) {
+//     alert('3+, 0-')
+// } else if ((a > 0 && b > 0 && c < 0) || (a > 0 && b < 0 && c > 0) || (a < 0 && b > 0 && c > 0)) {
+//     alert("2+, 1- ")
+// } else if ((a > 0 && b < 0 && c < 0) || (a < 0 && b > 0 && c < 0) || (a < 0 && b < 0 && c > 0)) {
+//     alert("1+, 2-")
+// } else {
+//     alert("0+, 3-")
+// }
+
+// let a = Number(prompt(`a=`))
+// let b = Number(prompt(`b=`))
+// if(a>b){alert(a)}
+// else if(a<b){alert(b)}
+// else{alert(`a=b`)}
+
+// let a = Number(prompt(`a=`))
+// let b = Number(prompt(`b=`))
+// if(a<b){alert(0)}
+// else if(a>b){alert(1)}
+// else{`a=b(0,1)`}
+
+// let a = Number(prompt(`a=`))
+// let b = Number(prompt(`b=`))
+// if(a>b){alert(`${a}, ${b}`)}
+// else if(a<b){alert(`${b}, ${a}`)}
+// else{alert(`a=b`)}
+
+// let a = Number(prompt(`a=`))
+// let b = Number(prompt(`b=`))
+// if(a>b){
+//     alert(`a=${b},
+// b=${a}`)
+// }else if(a<b){
+//     alert(`a=${a},
+// b=${b}`)
+// }else{alert(`a=b`)}
+
+// let a = Number(prompt(`a=`))
+// let b = Number(prompt(`b=`))
+// if(a!=b){
+//     alert(`a=${a+b}
+// b=${a+b}`)
+// }else if(a=b){
+//     alert(`a=${0}
+// b=${0}`)
+// }
+
+// let a = Number(prompt(`a=`))
+// let b = Number(prompt(`b=`))
+// if(a!=b){
+//     if(a>b){
+//         b=a
+//     }
+//     else if(a<b){
+//         a=b
+//     }
+//     alert(`a=${a},
+// b=${b}`)
+// }
+// else if(a=b){
+//     a=0
+//     b=0
+//     alert(`a=${a},
+// b=${b}`)
+// }
+
+// let a = Number(prompt(`a=`))
+// let b = Number(prompt(`b=`))
+// let c = Number(prompt(`c=`))
+// if(a<b && a<c){alert(`a=${a}`)}
+// else if(b<a && b<c){alert(`b=${b}`)}
+// else if(c<b && c<a){alert(`c=${c}`)}
+// else{alert(`EROR404`)}
+
+// let a = Number(prompt(`a=`))
+// let b = Number(prompt(`b=`))
+// let c = Number(prompt(`c=`))
+// if((a>b && a<c)||(a<b && a>c)){alert(`a=${a}`)}
+// else if((b>a && b<c) || (b<a && b>c)){alert(`b=${b}`)}
+// else if((c>b && c<a) || (c<b && c>a)){alert(`c=${c}`)}
+// else{alert(`EROR404`)}
+
+//task14
+// let a = Number(prompt("a="))
+// let b = Number(prompt("b="))
+// let c = Number(prompt("c="))
+// if (a > b && b > c){
+//     alert(c)
+//     alert(a)
+// } else if (a > c && c > b) {
+//     alert(b)
+//     alert(a)
+// } else if (b > a && a > c) {
+//     alert(c)
+//     alert(b)
+// } else if (b > c && c > a) {
+//     alert(a)
+//     alert(b)
+// } else if (c > a && a > b) {
+//     alert(b)
+//     alert(c)
+// } else {
+//     alert(a)
+//     alert(c)
+// }
+
+//task15
+// let a = Number(prompt("a="))
+// let b = Number(prompt("b="))
+// let c = Number(prompt("c="))
+// if (a > b && b > c){
+//     alert(a+b)
+// } else if (a > c && c > b) {
+//     alert(a+c)
+// } else if (b > a && a > c) {
+//     alert(c)
+// } else if (b > c && c > a) {
+//     alert(b+c)
+// } else if (c > a && a > b) {
+//     alert(c+a)
+// } else {
+//     alert(b+c)
+// }
+
+//task16
+let a = Number(prompt("a="))
+let b = Number(prompt("b="))
+let c = Number(prompt("c="))
+if (a < b && b < c) {
+    alert(`a=${2*a}`)
+    alert(`b=${2*b}`)
+    alert(`c=${2*c}`)
+} else {
+    alert(`a=${-a}`)
+    alert(`b=${-b}`)
+    alert(`c=${-c}`)
+}
+
+
+
+
+
+
+
+
+
